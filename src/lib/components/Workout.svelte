f1a4b580a8b2cae239a9e874c78bd3a62c46f283
DaanKorver <daank2000@hotmail.nl>
2022-09-12 13:21:23 +0200

    Fixed unused code

--- src/lib/components/Workout.svelte
+++ src/lib/components/Workout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import ExerciseCard from '$lib/components/Slider/ExerciseCard.svelte';
 	export let workoutTitle: string;
 </script>
 
 <section>
 	<h2>{workoutTitle}</h2>
-	<ExerciseCard exercise="Deadlift" setCount={3} repCount={8} restCount={340} />
-	<ExerciseCard exercise="Lat Pulldown" setCount={3} repCount={8} restCount={340} />
-	<ExerciseCard exercise="Bicep Curls" setCount={3} repCount={8} restCount={340} />
-	<ExerciseCard exercise="Rear Delt Flyes" setCount={3} repCount={8} restCount={340} />
 </section>
 
 <style>
